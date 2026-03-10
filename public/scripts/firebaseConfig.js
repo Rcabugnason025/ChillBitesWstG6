44e8a366503e7f00f4eff1254345c80f2cdcfe9e
your_github_username <your_github_email>
2026-03-10 00:06:58 -0500

    feat: update Firebase credentials to ChillBites-Final

--- public/scripts/firebaseConfig.js
+++ public/scripts/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "AIzaSyC6TkuNurP69yz9uXHoWQcgsnP3S9fLmxY",
-  authDomain: "device-streaming-e8feade3.firebaseapp.com",
-  projectId: "device-streaming-e8feade3",
-  storageBucket: "device-streaming-e8feade3.firebasestorage.app",
-  messagingSenderId: "787944808515",
-  appId: "1:787944808515:web:72eb167a6b9b2132dcf4f6"
+  apiKey: "AIzaSyD0lJvmnyrqtcLiQybWkzUHQf12_sEcb6U",
+  authDomain: "chillbites-final.firebaseapp.com",
+  projectId: "chillbites-final",
+  storageBucket: "chillbites-final.firebasestorage.app",
+  messagingSenderId: "420588741348",
+  appId: "1:420588741348:web:44dcf02121d084f5bc02f1"
 };
