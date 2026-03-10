12cbc9fcab34705fb26cb7a6cdbd9397c674932b
your_github_username <your_github_email>
2026-03-09 23:48:51 -0500

    feat: configure Firebase credentials (MS2)

--- public/scripts/firebaseConfig.js
+++ public/scripts/firebaseConfig.js
@@ -1,11 +1,8 @@
-// TODO: PASTE YOUR FIREBASE CONFIGURATION HERE
-// 1. Go to Firebase Console -> Project Settings -> General -> Your apps -> SDK setup and configuration -> Config
-// 2. Copy the values and paste them below
 export default {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE"
+  apiKey: "AIzaSyC6TkuNurP69yz9uXHoWQcgsnP3S9fLmxY",
+  authDomain: "device-streaming-e8feade3.firebaseapp.com",
+  projectId: "device-streaming-e8feade3",
+  storageBucket: "device-streaming-e8feade3.firebasestorage.app",
+  messagingSenderId: "787944808515",
+  appId: "1:787944808515:web:72eb167a6b9b2132dcf4f6"
 };
